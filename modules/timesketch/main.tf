b0678ee6a3cb1ebd72bb8957da8484ec843bb258
Zawadi <hi@zawadidone.nl>
2022-03-25 12:24:04 +0100

    Edit variable

--- modules/timesketch/main.tf
+++ modules/timesketch/main.tf
@@ -241,7 +241,7 @@ data "template_file" "worker" {
   vars = {
     timesketch_configuration = data.template_file.configuration.rendered
     timesketch_version                   = var.timesketch_version
-    timesketch_admin_password = random_string.timesketch_admin_password.result
+    timesketch_admin_password = var.timesketch_password 
     file_share_ip_address     = google_filestore_instance.default.networks[0].ip_addresses[0]
     file_share_name           =  google_filestore_instance.default.file_shares[0].name
   }
